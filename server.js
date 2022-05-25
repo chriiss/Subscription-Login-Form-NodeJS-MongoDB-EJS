d02e706d929f50931dffe0b2152702093a1c7d7b
chriiss <christopher.kumla@gmail.com>
2022-05-25 15:47:04 +0200

    create signup backend

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const connectLiveReload = require("connect-livereload");
 const path = require("path");
 const liveReloadServer = livereload.createServer();
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/my_db');
+const db = mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', (callback) => {
+   console.log("connection mongodb ok!");
+})
+const User = require("./models/user.model");
+const bcrypt = require("bcrypt");
 
 
 liveReloadServer.watch(path.join(__dirname, 'public'))
@@ -24,9 +32,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/public',express.static('public'));
 app.set('view engine', 'ejs');
 
+
+/* Routes */
+
 app.get('/', (req, res) => {
   res.render('home');
-})
+}).listen(3000)
 
 
 app.get('/login', (req, res) => {
@@ -38,14 +49,21 @@ app.get('/register', (req, res) => {
     res.render('register');
 })
 
-mongoose.connect('mongodb://localhost/my_database')
-.then(()=>{
-    app.listen(port, ()=>{
-        console.log("Database connection is Ready "
-        + "and Server is Listening on Port ", port);
-    })
-})
-.catch((err)=>{
-    console.log("A error has been occured while"
-        + " connecting to database.");
+app.post('/register', async (req, res, next) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let data = {
+        "name": name,
+        "email": email,
+        "password": await bcrypt.hash(password, 10)
+    }
+
+    db.collection('signup').insertOne(data,function(err, collection){
+        if (err) throw err;
+           console.log("Record inserted Successfully");
+        });
+        return res.render('success');
+
 })
